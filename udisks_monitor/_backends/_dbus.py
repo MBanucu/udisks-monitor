68fb5aa50feb54ab50af32ec80c7ffeb076199a4
MBanucu <michael.banucu@googlemail.com>
2026-06-23 22:14:42 +0200

    debug: log dispatch to trace JobCompleted path

--- udisks_monitor/_backends/_dbus.py
+++ udisks_monitor/_backends/_dbus.py
@@ -170,6 +170,7 @@ class _DBusBackend(_Backend):
             if event_type is None or isinstance(event, event_type):
                 q.put(event)
         self._publish(event)
+        print(f'  [DISP] {type(event).__name__} subs={len([s for _,e,_ in self._subs if e is None or isinstance(event, e)])}')
 
     def _on_interfaces_added(self, object_path, ifaces):
         ts = _timestamp()
